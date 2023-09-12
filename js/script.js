831f4aed368f2dbbb6566f69afaf26b396f7ef22
Afzal Momin <afzalmomin2003@gmail.com>
2023-09-12 09:00:05 +0530

    Added Swiper now playing animation

--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ function router() {
     case "/":
     case "/index.html":
         displayPopularMovies();
+        displaySlider();
       break;
     case "/movie-details.html":
       displayMovieDetails();
       break;
     case "/search.html":
-      console.log("Search");
+      ;
       break;
     case "/shows.html":
       displayPopular_TvShows();
@@ -254,4 +255,47 @@ function backgroundImage(type,path){
   } else {
     document.querySelector("#show-details").appendChild(overlayDiv);
   }
+}
+
+// swipper slider 
+
+async function displaySlider() {
+  const { results } = await FetchApiData("movie/now_playing")
+  results.forEach((movie) => {
+  const div = document.createElement("div");
+  div.classList.add("swiper-slide")
+  div.innerHTML = `
+  <a href="movie-details.html?id=${movie.id}">
+    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+  </a>
+  <h4 class="swiper-rating">
+    <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+  </h4>`
+  document.querySelector(".swiper-wrapper").appendChild(div);
+  initSwiper();
+  });  
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
 }
