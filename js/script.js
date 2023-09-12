9f7b49dcb7937f4c71c9e1c351547a7ad319ed07
Afzal Momin <afzalmomin2003@gmail.com>
2023-09-12 05:39:58 +0530

    Added Movies Details section

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function router() {
         displayPopularMovies();
       break;
     case "/movie-details.html":
-      console.log("Movie Details");
+      displayMovieDetails();
       break;
     case "/search.html":
       console.log("Search");
@@ -20,7 +20,7 @@ function router() {
       displayPopular_TvShows();
       break;
     case "/tv-details.html":
-      console.log("TV Details");
+      ;
       break;
   }
   highlightactivelink();
@@ -36,6 +36,7 @@ function highlightactivelink() {
 }
 
 // fetch Api Data 
+
 async function FetchApiData (endpoint) {
     API_url = "https://api.themoviedb.org/3/"
     API_key = "76e353fa85bd1df2f7b5793d2b5dfeaa"
@@ -47,6 +48,7 @@ async function FetchApiData (endpoint) {
 }
 
 // diplay popular movies 
+
 async function displayPopularMovies (){
     const {results} = await FetchApiData('movie/popular');
     results.forEach( movie => {
@@ -68,6 +70,64 @@ async function displayPopularMovies (){
         </div>`
         document.querySelector('#popular-movies').appendChild(div)
     })
+}
+
+// display movie details
+
+async function displayMovieDetails () {
+  const movieId = window.location.search.split("=")[1];
+  const movie = await FetchApiData(`movie/${movieId}`);
+  console.log(movie)
+  const div = document.createElement("div");
+  div.innerHTML = `<div class="details-top">
+  <div>
+    ${
+      movie.poster_path ?`
+    <a href="movie-details.html?id=${movie.id}">
+            <img
+          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+    :`<img
+      src="../images/no-image.jpg"
+      class="card-img-top"
+      alt="Movie Title"
+    />`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">${movie.genres.map((movie) => `<li>${movie.name}</li>`).join(" ")}
+    </ul>
+    <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+    <li><span class="text-secondary">Runtime:</span> ${addCommasToNumber(movie.runtime)} minutes</li>
+    <li><span class="text-secondary">Status:</span> ${addCommasToNumber(movie.status)}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">${movie.production_companies.map((company) => company.name).join(" , ")}</div>
+</div>` 
+document.querySelector("#movie-details").appendChild(div);
+}
+// Regex for adding commas to numbers
+
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 // display popular tv shows
