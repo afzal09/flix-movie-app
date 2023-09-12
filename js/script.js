0368eeee998903d65b2447533e0e9d762f77f6f4
Afzal Momin <afzalmomin2003@gmail.com>
2023-09-12 08:29:22 +0530

    Added Show details

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function router() {
       displayPopular_TvShows();
       break;
     case "/tv-details.html":
-      ;
+      displayShowDetails();
       break;
   }
   highlightactivelink();
@@ -77,13 +77,12 @@ async function displayPopularMovies (){
 async function displayMovieDetails () {
   const movieId = window.location.search.split("=")[1];
   const movie = await FetchApiData(`movie/${movieId}`);
-  console.log(movie)
+  backgroundImage('movie', movie.backdrop_path);
   const div = document.createElement("div");
   div.innerHTML = `<div class="details-top">
   <div>
     ${
       movie.poster_path ?`
-    <a href="movie-details.html?id=${movie.id}">
             <img
           src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
           class="card-img-top"
@@ -124,6 +123,76 @@ async function displayMovieDetails () {
 </div>` 
 document.querySelector("#movie-details").appendChild(div);
 }
+
+// display show details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await FetchApiData(`tv/${showId}`);
+
+  // Overlay for background image
+  backgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    show.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.name}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Last Episode To Air:</span> ${
+      show.last_episode_to_air.name
+    }</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${show.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(', ')}
+  </div>
+</div>
+  `;
+
+  document.querySelector('#show-details').appendChild(div);
+}
 // Regex for adding commas to numbers
 
 function addCommasToNumber(number) {
@@ -138,7 +207,7 @@ async function displayPopular_TvShows (){
       const div = document.createElement('div');
       div.classList.add('card');
       div.innerHTML = `
-      <a href="movie-details.html?id=${tv.id}">
+      <a href="tv-details.html?id=${tv.id}">
           <img
         src="https://image.tmdb.org/t/p/w200${tv.poster_path}"
         class="card-img-top"
@@ -156,9 +225,33 @@ async function displayPopular_TvShows (){
 }
 
 // spinner animation
+
 function ShowSpinner() {
   document.querySelector(".spinner").classList.add("show");
 }
+
 function HideSpinner() {
   document.querySelector(".spinner").classList.remove("show");
+}
+
+// background overlay image
+
+function backgroundImage(type,path){
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${path})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.2';
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
